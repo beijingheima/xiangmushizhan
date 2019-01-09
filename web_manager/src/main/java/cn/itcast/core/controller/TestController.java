e53263ddcc5b7edb7999ed183f1e3e1d35a29ab8
lijian <928036665@qq.com>
2019-01-09 14:48:13 +0800

    1

---
+++ web_manager/src/main/java/cn/itcast/core/controller/TestController.java
@@ -0,0 +1,9 @@
+package cn.itcast.core.controller;
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+    }
+}
