37ccbc1a53f4cf1aa8b1ea982fb9674bb9ae1237
lijian <928036665@qq.com>
2019-01-09 15:05:36 +0800

    删除测试

--- web_manager/src/main/java/cn/itcast/core/controller/TestController.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.core.controller;
-
-public class TestController {
-    public static void main(String[] args) {
-        System.out.println("1111");
-        System.out.println("1111");
-        System.out.println("1111");
-    }
-}
